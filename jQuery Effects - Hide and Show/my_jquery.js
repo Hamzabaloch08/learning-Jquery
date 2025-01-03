1bcdd1a6738849d2ad6ec96c68a98a08b0c520d5
Hamzabaloch08 <hb.developer08@gmail.com>
2025-01-03 14:38:35 +0500

    learn jQuery Effects - Hide and Show

---
+++ jQuery Effects - Hide and Show/my_jquery.js
@@ -0,0 +1,12 @@
+// $('#show').click(function () { 
+//     $('#box').show(1000);
+// });
+
+// $('#hide').click(function () { 
+//     $('#box').hide(1000);    
+// });
+
+
+$("button").click(function(){
+    $("#box").toggle(1000);
+  });
